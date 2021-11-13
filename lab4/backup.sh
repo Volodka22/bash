d24f3fefca1ce0590f352dd69533916bc661ab8f
root <root@localhost.localdomain>
2021-11-14 00:04:46 +0300

    finish backup

--- lab4/backup.sh
+++ lab4/backup.sh
@@ -2,34 +2,43 @@
 
 HOME_DIR="/home/user/"
 
-last="0"
+last=1
 
 cd $HOME_DIR
 
-ls -d Backup-* 2> /dev/null | while read line;
+while read line;
 do
-	echo $line
-	it_data=${$line%"Backup-"}
+	it_data=$(echo $line | sed -e "s/^Backup-//")
 	cur=$(date +%s -d $it_data)
-	echo cur
 	if [ $last -lt $cur ]
 	then
 		last=$cur
 	fi
-done
+done <<<$(ls -d Backup-* 2> /dev/null)
 let DIFF=($(date +%s)-$last)/86400
 if [ $DIFF -ge 7 ]
 then
-	cp -r ./source ./Backup-$(date +'%Y-%m-%d')
+	cp -a ./source/. ./Backup-$(date +'%Y-%m-%d')/
 else
-	last=$(date +'%Y-%m-%d' -d $last)
-	flag="false"
+	last=$(date +'%Y-%m-%d' -d @$last)
+	#echo $last
 	for file in ./source/*; do
-		if [[ -f ./Backup-$last/$file ]]; then
-			flag="true"
-			if [[ $(wc -c ./Backup-$last/$file) -eq $(wc ) ]]
+	#	echo $file
+		name=$(echo $file | sed -e "s/^.\/source\///")
+	#	echo $name
+	#	echo ./Backup-$last/$name
+		if [[ -f ./Backup-$last/$name ]]; then
+			new_c=$(wc -c ./Backup-$last/$name | awk '{print $1}')
+			old_c=$(wc -c $file | awk '{print $1}')
+	#		echo $new_c
+	#		echo $old_c
+			if [[ $new_c -ne $old_c ]]
+			then
+				mv ./Backup-$last/$name ./Backup-$last/$name.$(date +"%Y-%m-%d")
+				cp $file ./Backup-$last/$name
+			fi
 		else
-			cp ./source/file ./Backup-$last/$file
+			cp $file ./Backup-$last/$name
 		fi
 	done
 fi
