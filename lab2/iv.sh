b7352aca49ee6680aba9e8edb0233226f80497bc
root <root@localhost.localdomain>
2021-11-13 12:43:44 +0300

    add 3 lab

--- lab2/iv.sh
+++ lab2/iv.sh
@@ -5,10 +5,10 @@ cd /proc
 for i in $(ls -d -1 [0-9]*)
 do
 #if test -d "/proc/$i"; then
-		PPid=`cat  /proc/$i/status 2>/dev/null | awk '$1=="PPid:"{print $2}'`
+		PPid=`cat  /proc/$i/status 2>/dev/null | awk '$1 == "PPid:"{print $2}'`
 		SUM=`cat /proc/$i/sched 2>/dev/null | awk '$1 == "se.sum_exec_runtime"{print $3}'`
 		NR=`cat /proc/$i/sched 2>/dev/null | awk '$1 == "nr_switches"{print $3}'`
-		if [[ !( -z NR)  ]]
+		if [[ ! -z "$NR"  ]]
 		then
 			ART=$(echo "scale=5 ; $SUM / $NR" | bc)
 			echo "ProcessID="$i" : Parent_ProcessID="$PPid" : Average_Running_Time="$ART
